48d6a402822fbf1cbea2538e92838b2c115758e9
Abdullah Bayindir <byndrabdullah@gmail.com>
2019-09-23 20:56:31 -0500

    pissed off linter to test travis ci

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -10,6 +10,11 @@ module.exports = function(app) {
 
   // Create a new example
   app.post("/api/examples", function(req, res) {
+
+
+
+
+    
     db.Example.create(req.body).then(function(dbExample) {
       res.json(dbExample);
     });
